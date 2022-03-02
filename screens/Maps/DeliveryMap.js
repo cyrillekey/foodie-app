7020564daedcc156b2e9c25197718032c4bad728
cyrille <cyrilleotieno7@gmail.com>
2022-03-02 13:18:58 +0300

    added map

--- screens/Maps/DeliveryMap.js
+++ screens/Maps/DeliveryMap.js
@@ -1,46 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text ,StyleSheet} from 'react-native'
 import React from 'react'
-import MapView,{PROVIDER_GOOGLE} from "react-native-maps"
+import MapView,{PROVIDER_GOOGLE,Marker} from "react-native-maps"
 import { COLORS } from '../../constants';
 const DeliveryMap = () => {
-        const mapView=React.createRef()
-        const [region,setRegion] = React.useState(null);
-        const [toLoc,setToLoc] = React.useState(null);
-        const [fromLoc,setFromLoc] = React.useState(null);
-        const [angle,setAngle] = React.useState(0)
-        React.useEffect(() => {
-            let initialRegion = {
-                latitude: 1.5496614931250685,
-                longitude: 110.36381866919922,
-                latitudeDelta: 0.02,
-                longitudeDelta: 0.02
-            }
-    
-            let destination = {
-                latitude: 1.5496614931250685,
-                longitude: 110.36381866919922,
-            }
-            setToLoc(destination)
-            setFromLoc(destination)
-            setRegion(initialRegion)
-        }, [])
-    
   return (
-    <View
-    style={{
-        flex:1,
-        borderWidth:2,
-    }}
-    >
-      <MapView
-      ref={mapView}
-      provider={PROVIDER_GOOGLE}
-      initialRegion={region}
-      >
-    
-      </MapView>
-    </View>
+    <View style={styles.mapcontainer}>
+   <MapView
+       provider={PROVIDER_GOOGLE}
+       style={styles.map}
+       showUserLocation={true} >
+   </MapView>
+</View>
   )
 }
-
+const styles = StyleSheet.create({
+  mapcontainer: {
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+  },
+  map: {
+        ...StyleSheet.absoluteFillObject,
+  },
+})
 export default DeliveryMap
