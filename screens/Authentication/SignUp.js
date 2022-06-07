1fbf639bac41969bb7408bc4ed0b4341b3668aea
cyrille <cyrilleotieno7@gmail.com>
2022-06-07 10:57:33 +0300

    connecting to backend

--- screens/Authentication/SignUp.js
+++ screens/Authentication/SignUp.js
@@ -5,7 +5,41 @@ import { Text, View } from 'react-native';
 import { AuthLayout } from '..';
 import { FormInput, TextButton } from '../../Components';
 import { SIZES ,FONTS,COLORS} from '../../constants';
+import axios from 'axios';
 const SignUp=({navigation})=>{
+    const [form,setForm]=React.useState({
+        email:"",
+        phone:"",
+        password:""
+    });
+    const signUp=()=>{
+        var data = JSON.stringify({
+              "user_name": form.email,
+              "user_mail":form.email,
+              "password": form.password,
+              "user_phone": form.phone,
+              "accountType": "CUSTOMER"
+          });
+          
+          var config = {
+            method: 'post',
+            url: "https://foodieback.herokuapp.com/register",
+            headers: { 
+              'Content-Type': 'application/json'
+            },
+            data : data
+          };
+          
+          axios(config)
+          .then(function (response) {
+            if(response.status=="201"){
+                navigation.navigate("Otp");
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
     return (
         <AuthLayout
             title="Sign Uo"
@@ -24,7 +58,7 @@ const SignUp=({navigation})=>{
                     autoCompleteType="email"
                     errorMsg="invalid email"
                     onChange={(text)=>{
-                        console.log(text);
+                        setForm({...form,email:text});
                     }}
                     containerStyle={{
                         marginTop:SIZES.padding,
@@ -36,7 +70,7 @@ const SignUp=({navigation})=>{
                 autoCompleteType="tel"
                 keyboardType="phone-pad"
                 onChange={(text)=>{
-                    console.log(text);
+                    setForm({...form,phone:text});
                 }}
                 containerStyle={{
                     marginTop:SIZES.padding,
@@ -47,7 +81,7 @@ const SignUp=({navigation})=>{
                 placeholder="********"
                 secureTextEntry={true}
                 onChange = {(value)=>{
-                    console.log(value);
+                    setForm({...form,password:value});
                 }}
                 containerStyle={{
                     marginTop:SIZES.padding,
@@ -62,7 +96,8 @@ const SignUp=({navigation})=>{
                         marginTop:SIZES.padding,
                     }}
                     onPress={()=>{
-                        navigation.navigate('Otp');
+                        signUp()
+                        //navigation.navigate('Otp');
                     }}
                 />
                 <View
