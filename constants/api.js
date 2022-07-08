067e9fa5831eae45a3bf352e7517bbd326bd48da
cyrille <cyrilleotieno7@gmail.com>
2022-07-08 11:11:20 +0300

    push notification navigation

--- constants/api.js
+++ constants/api.js
@@ -1,2 +1,2 @@
-//export const API_ENDPOINT = 'https://foodieback.herokuapp.com';
-export const API_ENDPOINT = 'https://1878-105-162-18-150.ngrok.io';
+export const API_ENDPOINT = 'https://foodieback.herokuapp.com';
+//export const API_ENDPOINT = 'https://1878-105-162-18-150.ngrok.io';
